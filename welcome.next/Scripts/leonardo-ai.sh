edb215ebdb2d5050508623798df7e273e565d66b
Natanael <natanaelgplay@gmail.com>
2024-12-05 16:53:07 -0300

    Corrige os webapps

--- welcome.next/Scripts/leonardo-ai.sh
+++ welcome.next/Scripts/leonardo-ai.sh
@@ -1,11 +1,20 @@
 
+url="https://app.leonardo.ai/"
+fname="leonardo-ai"
+
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
 Terminal=false
 Type=Application
 Name=Leonardo.AI
-Exec=webapp-player https://app.leonardo.ai/
+Exec=/usr/local/bin/${fname}
 Categories=Utility;
-Icon=/usr/share/tiger-resources/leonardo-ai.svg" > /usr/share/applications/leonardo-ai.desktop
+StartupWMClass=${url}
+Icon=/usr/share/tiger-resources/leonardo-ai.svg" > /usr/share/applications/${fname}.desktop
 
+mkdir -p /usr/local/bin/
+echo "#!/usr/bin/env bash
+webapp-player ${url}
+" > /usr/local/bin/${fname}
+chmod +x /usr/local/bin/${fname}
--- welcome.next/Scripts/maritalk.sh
+++ welcome.next/Scripts/maritalk.sh
@@ -1,11 +1,20 @@
 
+url="https://chat.maritaca.ai/"
+fname="maritalk"
+
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
 Terminal=false
 Type=Application
 Name=MariTalk
-Exec=webapp-player https://chat.maritaca.ai/
+Exec=/usr/local/bin/${fname}
 Categories=Utility;
-Icon=/usr/share/tiger-resources/maritalk.png" > /usr/share/applications/maritalk.desktop
+StartupWMClass=${url}
+Icon=/usr/share/tiger-resources/maritalk.png" > /usr/share/applications/${fname}.desktop
 
+mkdir -p /usr/local/bin/
+echo "#!/usr/bin/env bash
+webapp-player ${url}
+" > /usr/local/bin/${fname}
+chmod +x /usr/local/bin/${fname}
